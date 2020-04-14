fae34d1919572aee728aa7b8cad57eceac89e7a1
Shahnovski <shahnovski300899@gmail.com>
2020-04-14 19:39:46 +0300

    Initial commit

---
+++ src/main/java/com/example/autoshopserver/AutoshopServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoshopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoshopServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoshopServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/autoshopserver/AutoshopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoshopserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoshopServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
